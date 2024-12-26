9dd4802ef596cab1d5125d6c476db8841763625e
mayom <matthieu.sirier@efrei.net>
2024-12-26 16:40:16 +0100

    Bataille navale terminée, code un peu bizarre vu que j'ai pas tout suivi les guidelines, mais normalement tout fonctionne.

--- logical_challenges.py
+++ logical_challenges.py
@@ -196,4 +196,134 @@ def tictactoe_game(): # Operates the whole game
 
     return False
 
-print(tictactoe_game())
+"""
+Battleship game (High)
+"""
+
+def next_player(player):
+    return [1,0][player] # Flex way to switch between 0 and 1
+
+def empty_grid():
+    return [[" ", " ", " "], # Initialize the grid
+            [" ", " ", " "],
+            [" ", " ", " "]]
+
+def display_grid_ship(grid, message): # Display function
+    print(message)
+    print(f"{grid[0][0]} | {grid[0][1]} | {grid[0][2]}\n{grid[1][0]} | {grid[1][1]} | {grid[1][2]}\n{grid[2][0]} | {grid[2][1]} | {grid[2][2]}")
+    print("---------")
+
+def ask_position():
+
+    play = input("Entrez la position (ligne, colonne) entre 1 et 3 (ex: 1,2) : ")
+    valid = 0
+
+    while not valid:  # Verify that the input is correct
+
+        valid = (len(play) == 3 and play[0] in '123' and play[1] == ',' and play[2] in '123')  # Verify that the input is the correct format
+        if not valid: # If input is incorrect, ask gain
+            print("Position incorrecte, essayez encore :")
+            play = input("Entrez la position (ligne, colonne) entre 1 et 3 (ex: 1,2) : ")
+
+    return int(play[0]), int(play[2])
+
+def initialize():
+
+    boats = 2
+    grid = empty_grid()
+
+    while boats > 0: # While there is a boat to place
+
+        # Ask the player where he wants to place his boat
+        print("Où voulez-vous placer votre bateau ?")
+        print("Bateau ", 3-boats) # Boat number
+        coords = ask_position()
+
+        if grid[coords[0]-1][coords[1]-1] == " ": # If the emplacement is free, place a boat
+            grid[coords[0]-1][coords[1]-1] = "B"
+            boats -= 1
+
+        else:
+            print("Emplacement incorrect, essayez encore.")
+
+    display_grid_ship(grid, "Voici la grille de vos bateaux")
+    return grid
+
+def has_won(grid):
+    return sum(grid[i][j] == "X" for i in range(3) for j in range(3)) == 2 # Check if there is two cross, is yes then there is a win
+
+def turn(player, player_grid, player_shots_grid, opponent_grid): # Added player_grid, idk how else
+
+    if player:
+
+        print("Au maitre de jouer.")
+
+        places = [(i,j) for i in range(3) for j in range(3) if player_grid[i][j] in [" ", "B"]] # Moves the master didnt already picked
+        pick = places[randint(0, len(places)-1)] # Pick random place to shot
+        print(f"Le maître tire en {pick[0]+1},{pick[1]+1}")
+
+        if player_grid[pick[0]][pick[1]] == "B": # If the master shot a boat, place an X on the player grid
+            player_grid[pick[0]][pick[1]] = "X"
+            print("Touché ! Vous perdez un bateau.")
+
+        else: # If the master shot a boat, place a dot (.) on the player grid
+            player_grid[pick[0]][pick[1]] = "."
+            print("Plouf, le maître vous a raté.")
+
+        display_grid_ship(player_grid, "Voici la grille de vos bateaux")
+
+    else:
+
+        print("A vous de jouer.")
+        pos = ask_position()
+        print(f"Vous tirez en {pos[0]},{pos[1]}")
+
+        if opponent_grid[pos[0]-1][pos[1]-1] == "B": # If you shot a boat, add an X on your shot grid
+            player_shots_grid[pos[0]-1][pos[1]-1] = "X"
+            print("Touché ! Vous coulez un bateau.")
+
+        else: # If you missed, add a dot on your shot grid
+            player_shots_grid[pos[0]-1][pos[1]-1] = "."
+            print("Plouf... vous avez raté.")
+
+        display_grid_ship(player_shots_grid, "Voici la grille de vos tirs")
+
+def master_initialize():
+
+    grid = empty_grid()
+    places = [(i, j) for i in range(3) for j in range(3)] # Every usable place for the boats
+    boat1 = places.pop(randint(0, len(places)-1)) # Take a random localisation and removes it from the usable places
+    boat2 = places[randint(0, len(places)-1)] # Take a random localisation from the usable ones
+
+    # Places the boats on the randomly chosen positions
+    grid[boat1[0]][boat1[1]] = "B"
+    grid[boat2[0]][boat2[1]] = "B"
+
+    return grid
+
+def battleship_game():
+
+    print("Vous et le maître allez chacun avoir une grille 3x3 où seront placés 2 bateaux.\n"
+          "Chacun votre tour, vous aller entrer des coordonnées pour tenter de couler les bateaux adverses.\n"
+          "Le premier qui coule les deux bateaux de son adversaire remporte la victoire.")
+
+    # Initialise grids for the player and the master
+    player_grid = initialize()
+    player_shots_grid = empty_grid()
+    master_grid = master_initialize()
+
+    player = 0 # 0:Player, 1:Master
+
+    while not (has_won(player_grid) or has_won(player_shots_grid)): # While none has won
+
+        turn(player, player_grid, player_shots_grid, master_grid) # Do a turn
+        player = next_player(player) # Switch player turn
+
+    if player: # Determine who won, the winner is the inverse from player since we switched after the end of the round
+        print("Vous avez coulé tous les bateaux du maître ! Vous remporter cette épreuve et obtenez un clé.")
+        return True
+    else:
+        print("Tous vos bateaux ont été coulés par le maître. Vous perdez cette épreuve et n'obtenez pas de clé.")
+        return False
+
+battleship_game()
